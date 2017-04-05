ec9702a4959899897f5fd37448e3ac95282a4a5a
robert5066 <robert@gmail.com>
2017-04-05 13:51:14 +0800

    base

---
+++ app/src/main/java/com/never/nikkaandroid/base/BaseFragment.java
@@ -0,0 +1,24 @@
+package com.never.nikkaandroid.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public abstract class BaseFragment extends Fragment {
+
+    public BaseFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(getContentView(), container, false);
+    }
+
+    public abstract int getContentView();
+}
