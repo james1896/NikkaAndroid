56a41a1a955533363e7381cf5eab5f3db95e0b04
robert5066 <robert@gmail.com>
2017-04-05 18:39:30 +0800

    自定义字体

--- app/src/main/java/com/never/nikkaandroid/MainActivity.java
+++ app/src/main/java/com/never/nikkaandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.never.nikkaandroid;
 
+import android.graphics.Typeface;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import com.never.nikkaandroid.base.BaseActivity;
 public class MainActivity extends BaseActivity implements TabLayout.OnTabSelectedListener{
 
     private TextView titleTextView;
+    private String[] titles = new String[]{"Nikka","","Profile"};
     @Override
     protected int getContentView() {
         return R.layout.activity_main;
@@ -25,7 +27,9 @@ public class MainActivity extends BaseActivity implements TabLayout.OnTabSelecte
 //        }
 
         titleTextView = (TextView) findViewById(R.id.toolbar_title);
-        titleTextView.setText("0");
+        titleTextView.setText(titles[0]);
+        Typeface type= Typeface.createFromAsset(getAssets(),"font/MarkerFelt.ttf");
+        titleTextView.setTypeface(type);
 
         //viewPage
         ViewPager viewPage = (ViewPager) findViewById(R.id.viewpage);
@@ -34,22 +38,22 @@ public class MainActivity extends BaseActivity implements TabLayout.OnTabSelecte
         //tablayout
         android.support.design.widget.TabLayout tabLayout = (android.support.design.widget.TabLayout) findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPage);
-        tabLayout.setOnTabSelectedListener(this);
+        tabLayout.addOnTabSelectedListener(this);
     }
 
     @Override
     public void onTabSelected(TabLayout.Tab tab) {
+        titleTextView.setText(titles[tab.getPosition()]);
 
         switch (tab.getPosition()){
             case 0:
 
-                titleTextView.setText("0");
                 break;
             case 1:
-                titleTextView.setText("1");
+
                 break;
             case 2:
-                titleTextView.setText("2");
+
                 break;
             default:
                 break;
