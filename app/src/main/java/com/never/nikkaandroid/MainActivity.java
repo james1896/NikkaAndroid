3955889861c7adbd74e591d472b11d3cf69dce02
robert5066 <robert@gmail.com>
2017-04-05 15:32:43 +0800

    base

--- app/src/main/java/com/never/nikkaandroid/MainActivity.java
+++ app/src/main/java/com/never/nikkaandroid/MainActivity.java
@@ -1,17 +1,25 @@
 package com.never.nikkaandroid;
 
-import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 
 import com.never.nikkaandroid.adpter.MainActivityAdpter;
+import com.never.nikkaandroid.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getContentView() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void init() {
+        //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            //透明状态栏
+//            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//            //透明导航栏1
+//            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+//        }
 
         ViewPager viewPage = (ViewPager) findViewById(R.id.viewpage);
         android.support.design.widget.TabLayout tabLayout = (android.support.design.widget.TabLayout) findViewById(R.id.tabLayout);
--- app/src/main/java/com/never/nikkaandroid/base/BaseActivity.java
+++ app/src/main/java/com/never/nikkaandroid/base/BaseActivity.java
@@ -3,14 +3,21 @@ package com.never.nikkaandroid.base;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.never.nikkaandroid.R;
 
-
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
+        setContentView(getContentView());
+
+        init();
     }
+
+    protected abstract int getContentView();
+
+    /**
+     * 初始化操作
+     */
+    protected abstract void init();
 }
