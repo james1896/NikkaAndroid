7c8b62c6e0c6cc39a1f16220e5e0e0df2d326dc1
robert5066 <robert@gmail.com>
2017-04-08 11:12:17 +0800

    headerview listview 组装

---
+++ app/src/main/java/com/never/nikkaandroid/adpter/BaseListAdapter.java
@@ -0,0 +1,35 @@
+package com.never.nikkaandroid.adpter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by toby on 08/04/2017.
+ */
+
+public class BaseListAdapter extends BaseAdapter {
+    public BaseListAdapter(){
+
+
+    }
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
--- app/src/main/java/com/never/nikkaandroid/me/MeFragment.java
+++ app/src/main/java/com/never/nikkaandroid/me/MeFragment.java
@@ -2,17 +2,25 @@ package com.never.nikkaandroid.me;
 
 
 import android.app.Fragment;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.never.nikkaandroid.R;
 import com.never.nikkaandroid.base.BaseFragment;
 import com.never.nikkaandroid.base.CanvasView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MeFragment extends BaseFragment {
+
+    // 模拟数据
+    private List<String> dataList = null;
 
 
     public MeFragment() {
@@ -23,17 +31,40 @@ public class MeFragment extends BaseFragment {
     public int getContentView() {
         return R.layout.fragment_me;
     }
-
+    CanvasView headerView;
     @Override
     protected void init() {
-        final CanvasView canvasView = (CanvasView) contentView.findViewById(R.id.canvasview);
-        final TextView textView = (TextView) contentView.findViewById(R.id.tetview);
-
-
+//        final CanvasView canvasView = (CanvasView) contentView.findViewById(R.id.canvasview);
         //用代码写布局
-        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) canvasView.getLayoutParams();
-        params.height = 550;
-        canvasView.setLayoutParams(params);
+//        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) canvasView.getLayoutParams();
+//        params.height = 550;
+//        canvasView.setLayoutParams(params);
+
+
+        //代码初始化view
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 550);
+        headerView  = new CanvasView(getContext());
+        headerView.setBackgroundColor(getResources().getColor(R.color.white));
+        headerView.setLayoutParams(params);
+
+
+        dataList = new ArrayList<String>();
+        // 初始化数据
+        for (int i = 0; i < 20; i++) {
+            dataList.add("第" + i + "条数据");
+        }
+        // 设置adapter(所在的activity,使用的显示样式,数据源)
+        ListAdapter adapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_list_item_1, dataList);
+
+      ListView  listview = (ListView) contentView.findViewById(R.id.meListView);
+        listview.setAdapter(adapter);
+        listview.addHeaderView(headerView);
+
+        // 绑定item点击事件
+//        listview.setOnItemClickListener(this);
+
+
 
 
 
