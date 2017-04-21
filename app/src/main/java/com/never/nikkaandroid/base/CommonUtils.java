12b0e489373181377afce619e66cda995519b769
robert5066 <robert@gmail.com>
2017-04-21 17:07:08 +0800

    自定义 生成唯一码

--- app/src/main/java/com/never/nikkaandroid/base/CommonUtils.java
+++ app/src/main/java/com/never/nikkaandroid/base/CommonUtils.java
@@ -49,18 +49,40 @@ public class CommonUtils {
     public static String getUniquePsuedoID(){
         String m_szDevIDShort = "35" +
 
-                Build.BOARD.length()%10 +
-                Build.BRAND.length()%10 +
+                //主板
+                Build.BOARD.length() % 10   +
+
+                //android系统定制商
+                Build.BRAND.length() % 10   +
+
+                //cpu指令集
                 Build.CPU_ABI.length() % 10 +
-                Build.DEVICE.length() %10 +
-                Build.DISPLAY.length() %10 +
-                Build.HOST.length() % 10 +
-                Build.ID.length() % 10 +
+
+                //设备参数
+                Build.DEVICE.length() % 10  +
+
+                //显示屏参数
+                Build.DISPLAY.length() % 10 +
+                Build.HOST.length() % 10    +
+
+                //修订版本列表
+                Build.ID.length() % 10      +
+
+                //硬件制造商
                 Build.MANUFACTURER.length() % 10 +
-                Build.MODEL.length() % 10 +
+
+                //版本
+                Build.MODEL.length() % 10   +
+
+                //手机制造商
                 Build.PRODUCT.length() % 10 +
-                Build.TAGS.length() % 10 +
-                Build.TYPE.length() % 10 +
+
+                //描述build的标签
+                Build.TAGS.length() % 10    +
+
+                //builder类型
+                Build.TYPE.length() % 10    +
+                //
                 Build.USER.length() % 10;
 
         String serial = Build.SERIAL;
