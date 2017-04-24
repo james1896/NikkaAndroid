5052d29a7fec16e537a6b6c55a4cf16bf21c9b9b
robert5066 <robert@gmail.com>
2017-04-24 18:48:38 +0800

    登录 UI

--- app/src/main/java/com/never/nikkaandroid/base/LoginActiviy.java
+++ app/src/main/java/com/never/nikkaandroid/base/LoginActiviy.java
@@ -1,9 +1,12 @@
 package com.never.nikkaandroid.base;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.never.nikkaandroid.R;
 
@@ -11,6 +14,10 @@ public class LoginActiviy extends AppCompatActivity implements View.OnClickListe
     private View layout;
     private View login;
     private View register;
+    private EditText login_user_edit ;
+    private EditText login_pwd_edit ;
+    private EditText register_user_edit ;
+    private EditText register_pwd_edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,25 +32,49 @@ public class LoginActiviy extends AppCompatActivity implements View.OnClickListe
         this.login.setVisibility(View.GONE);
         this.register.setVisibility(View.GONE);
 
-        Button back = (Button) findViewById(R.id.login_back);
-        Button loginBtn = (Button) findViewById(R.id.loginBtn);
-        Button registerBtn = (Button) findViewById(R.id.registerBtn);
+        //关闭键盘
+//        InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+//        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+
+        ImageView back = (ImageView) findViewById(R.id.login_back);
+        Button base_login_btn = (Button) findViewById(R.id.base_login_btn);
+        Button base_register_btn = (Button) findViewById(R.id.base_register_btn);
 
         back.setOnClickListener(this);
-        loginBtn.setOnClickListener(this);
-        registerBtn.setOnClickListener(this);
+        base_login_btn.setOnClickListener(this);
+        base_register_btn.setOnClickListener(this);
+
+
+        View main_layout = findViewById(R.id.mainLayout);
+        main_layout.setOnClickListener(this);
+
+        this.login_user_edit = (EditText) findViewById(R.id.login_user_edit);
+        this.login_pwd_edit = (EditText) findViewById(R.id.login_pwd_edit);
+        this.register_user_edit = (EditText) findViewById(R.id.register_user_edit);
+        this.register_pwd_edit = (EditText) findViewById(R.id.register_pwd_edit);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.loginBtn:{
+
+            //取消键盘
+            case R.id.mainLayout:
+                InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(this.login_user_edit.getWindowToken(), 0);
+                imm.hideSoftInputFromWindow(this.login_pwd_edit.getWindowToken(), 0);
+                imm.hideSoftInputFromWindow(this.register_user_edit.getWindowToken(), 0);
+                imm.hideSoftInputFromWindow(this.register_pwd_edit.getWindowToken(), 0);
+                break;
+
+            case R.id.base_login_btn:{
                 this.layout.setVisibility(View.GONE);
                 this.login.setVisibility(View.VISIBLE);
                 this.register.setVisibility(View.GONE);
                 break;
             }
-            case R.id.registerBtn:{
+
+            case R.id.base_register_btn:{
                 this.layout.setVisibility(View.GONE);
                 this.login.setVisibility(View.GONE);
                 this.register.setVisibility(View.VISIBLE);
@@ -64,8 +95,6 @@ public class LoginActiviy extends AppCompatActivity implements View.OnClickListe
                     this.login.setVisibility(View.GONE);
                     this.register.setVisibility(View.GONE);
                 }
-
-
                 break;
             }
 
