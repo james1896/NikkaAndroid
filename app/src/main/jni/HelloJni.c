bac8ac0e78db81da15f016706a4961ffd2d40acd
robert5066 <robert@gmail.com>
2017-04-27 21:04:12 +0800

    月日年

--- app/src/main/jni/HelloJni.c
+++ app/src/main/jni/HelloJni.c
@@ -55,34 +55,55 @@ JNIEXPORT jint JNICALL Java_com_never_nikkaandroid_base_JniHello_intArrayMethod
 JNIEXPORT jstring JNICALL Java_com_never_nikkaandroid_base_JniHello_serialWithUserID
         (JNIEnv *env, jobject obj, jint num)
 {
-
-    jint result_i = num;
-    char buf[64]; // assumed large enough to cope with result
-
-
-    jint first  = num/1000/1000;
-    jint second = num%1000000/1000;
-    jint third  = num%1000;
-
-    char firstBuf[6];
-    char secondBuf[6];
-    char thirdBuf[6];
-    char tmpBuf[6];
-
-    sprintf(firstBuf,"%d",first);
-    sprintf(secondBuf,"%d",second);
-    sprintf(thirdBuf,"%d",third);
-
-//    tmpBuf = firstBuf;
-//    firstBuf[0] = firstBuf[1];
-
     //得到当前秒数
     time_t t;
     time(&t);
     int t2 = t%100;
     int t3 = t%1000/100;
-//    sprintf(buf, "%d|%s|%s|%s+%3d",result_i,firstBuf,secondBuf,thirdBuf,rand()%100);  // error checking omitted
-//    sprintf(buf,"%s|%d|%d",timeBuf,t3,t2);
+    int i = t2%10;
+    //
+    jint result_i = num;
+    char buf[64]; // assumed large enough to cope with result
+
+    //得到 每三位一组
+    jint first  = num/1000/1000;
+    jint second = num%1000000/1000;
+    jint third  = num%1000;
+
+    //拆分成一位 一共九位
+    jint i1 = first/100;
+    jint j1 = first%100/10;
+    jint k1 = first%10;
+
+    jint i2 = second/100;
+    jint j2 = second%100/10;
+    jint k2 = second%10;
+
+    jint i3 = third/100;
+    jint j3 = third%100/10;
+    jint k3 = third%10;
+
+    //
+    char firstBuf[6];
+    char secondBuf[6];
+    char thirdBuf[6];
+    char tmpBuf[6];
+
+    //sort
+
+    if(i>5){
+        sprintf(firstBuf,"%d%d%d",k1,i1,j1);
+        sprintf(secondBuf,"%d%d%d",i2,k2,j2);
+        sprintf(thirdBuf,"%d%d%d",j3,k3,i3);
+    } else{
+        sprintf(firstBuf,"%d",first);
+        sprintf(secondBuf,"%d",second);
+        sprintf(thirdBuf,"%d",third);
+    }
+
+
+
+//    sprintf(buf,"%d%d%d%d%d%d%d%d%d",i1,j1,k1,i2,j2,k2,i3,j3,k3);
     sprintf(buf,"13%s%1d%1d%s%d%s%d%1d%1d",firstBuf,rand()%10,rand()%10,secondBuf,t2,thirdBuf,t3,rand()%10,rand()%10);
     return (*env)->NewStringUTF(env, buf);
 
