1673f76a0376d4b35ead67888e82e89ebd0f45c0
robert5066 <robert@gmail.com>
2017-05-03 14:41:40 +0800

    databinding

--- app/src/main/java/com/never/nikkaandroid/base/BaseActivity.java
+++ app/src/main/java/com/never/nikkaandroid/base/BaseActivity.java
@@ -1,17 +1,21 @@
 package com.never.nikkaandroid.base;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
+public abstract class BaseActivity<B extends ViewDataBinding> extends AppCompatActivity {
 
-public abstract class BaseActivity extends AppCompatActivity {
-
+    protected B dataBind;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(getContentView());
+//        setContentView(getContentView());
+        dataBind = DataBindingUtil.setContentView(this, getContentView());
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
             localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
